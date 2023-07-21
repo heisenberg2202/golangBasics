a4a6c9addcac2aa2b3a98e0c9306acbb020bc9d5
Ayush Narayan <ayushunplugged22@gmail.com>
2023-07-21 22:45:49 +0530

    Call Restful Api

---
+++ callrestfulapi/go.mod
@@ -0,0 +1,3 @@
+module callrestfulapi
+
+go 1.20
--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -6,6 +6,8 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// http://localhost:8080/albums
+
 // album represents data about a record album.
 type album struct {
     ID     string  `json:"id"`
